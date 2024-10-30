8e4dc7fb265bd49a18631fa4a2b543fd59c49a86
Myles Reid <myles@reidsmailbox.com>
2024-10-30 11:55:41 -0500

    Clock function added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,38 @@
 'use strict';
 
+function select(selector, scope = document) {
+  return scope.querySelector(selector);
+}
+
+function listen(event, selector, callback) {
+  return selector.addEventListener(event, callback);
+}
+
+const clockHours = select('.hours');
+const clockMinutes = select('.minutes');
+const outputTime = select('.time');
+const alarmTime = select('.alarm-set-time');
+const inputHours = select('.input-hours');
+const inputMinutes = select('.input-minutes');
+const setAlarmBtn = select('.set-alarm');
+const cancelAlarmBtn = select('.remove-alarm');
+
+let time = '';
+
+function getTime() {
+  const options = {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  }
+  const temp = new Date();
+  time = temp.toLocaleTimeString([], options);
+}
+
+function updateTime() {
+  getTime();
+  outputTime.innerText = time;
+}
+
+setInterval(updateTime, 1000);
